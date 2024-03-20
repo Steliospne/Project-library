1c761179956e51e05aecd478f6e5e0532ae9a3ef
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2024-03-20 16:22:41 +0100

    Changes the structrure from a factrory functions pattern to a class pattern

--- script.js
+++ script.js
@@ -1,99 +1,110 @@
-const myLibrary = [];
+class Book {
+    constructor(author, title, pages, read) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+        this.read = read;
+    }
 
-display = document.querySelector(".display");
-dialog = document.querySelector(".dialog");
-addBookButton = document.querySelector(".addBook");
-createBook = document.querySelector("#createBook");
-deleteBtnArray = document.getElementsByClassName("delBtn");
-
-function Book(author, title, pages, read) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
-    this.setIndex = (value) => {
+    setIndex(value) {
         this.index = value;
-    };
-}
+    }
 
-Book.prototype.readStatus = function () {
-    this.read = this.read == 0 ? "1" : "0";
-};
-
-addBookButton.addEventListener("click", () => {
-    dialog.showModal();
-});
-
-function readBtnStatus(book, readButton) {
-    if (book.read == "1") {
-        readButton.textContent = 'Read✓'
-    } else {
-        readButton.textContent = 'Read✗'  
+    readStatus() {
+        this.read = this.read == 0 ? "1" : "0";
     }
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-    book.index = `${myLibrary.indexOf(book)}`;
+class Library {
+    constructor() {
+        this.bookCase = [];
+    }
+
+    addBookToLibrary(obj) {
+        this.bookCase.push(obj);
+        obj.index = `${this.bookCase.indexOf(obj)}`;
+    }
 }
 
-function createBookElement(author, title, pages, book) {
-    let bookElement = document.createElement("div");
-    let titleElement = document.createElement("h3");
-    let descriptionElement = document.createElement("p");
-    let bookButtons = document.createElement("div");
-    let readButton = document.createElement("button");
-    let deleteButton = document.createElement("button");
+class screenUpdate {
+    constructor(document) {
+        this.display = document.querySelector(".display");
+        this.dialog = document.querySelector(".dialog");
+        this.addBookButton = document.querySelector(".addBook");
+        this.createBook = document.querySelector("#createBook");
+        this.deleteBtnArray = document.getElementsByClassName("delBtn");
 
-    bookButtons.className = "bookButton";
-    bookElement.className = "book";
-    readButton.textContent = "Read";
-    readButton.value = "0";
-    readBtnStatus(book, readButton)
-    deleteButton.value = book.index;
-    deleteButton.textContent = "Delete";
-    deleteButton.className = "delBtn";
-    titleElement.textContent = title;
-    descriptionElement.textContent = `Author: ${author}, Pages: ${pages}`;
-    bookButtons.append(readButton, deleteButton);
-    bookElement.append(titleElement, descriptionElement, bookButtons);
-    display.appendChild(bookElement);
+        this.addBookButton.addEventListener("click", () => {
+            this.dialog.showModal();
+        });
 
-    readButton.addEventListener("click", (e) => {
-        book.readStatus();
-        readBtnStatus(book, readButton)
-    });
+        this.createBook.addEventListener("click", (e) => {
+            e.preventDefault();
+            const author = document.querySelector("#author").value;
+            const title = document.querySelector("#title").value;
+            const pages = document.querySelector("#pages").value;
+            const readStatus = document.getElementsByName("read");
+            let read;
+            if (readStatus[0].checked) {
+                read = readStatus[0].value;
+            } else {
+                read = readStatus[1].value;
+            }
 
-    deleteButton.addEventListener("click", (e) => {
-        index = e.target.value;
-        myLibrary.splice(index, 1);
-        bookElement = document.querySelectorAll(".book");
-        bookElement[index].remove();
+            let newBook = new Book(author, title, pages, read);
+            myLibrary.addBookToLibrary(newBook);
+            this.createBookElement(author, title, pages, newBook);
+            this.dialog.close();
+        });
+    }
 
-        for (let i = 0; i < myLibrary.length; i++) {
-            myLibrary[i].index = i;
-            deleteBtnArray[i].value = i;
+    createBookElement(author, title, pages, obj) {
+        let bookElement = document.createElement("div");
+        let titleElement = document.createElement("h3");
+        let descriptionElement = document.createElement("p");
+        let bookButtons = document.createElement("div");
+        let readButton = document.createElement("button");
+        let deleteButton = document.createElement("button");
+        bookButtons.className = "bookButton";
+        bookElement.className = "book";
+        readButton.textContent = "Read";
+        readButton.value = "0";
+        this.readBtnStatus(obj, readButton);
+        deleteButton.value = obj.index;
+        deleteButton.textContent = "Delete";
+        deleteButton.className = "delBtn";
+        titleElement.textContent = title;
+        descriptionElement.textContent = `Author: ${author}, Pages: ${pages}`;
+        bookButtons.append(readButton, deleteButton);
+        bookElement.append(titleElement, descriptionElement, bookButtons);
+        this.display.append(bookElement);
+
+        readButton.addEventListener("click", () => {
+            obj.readStatus();
+            this.readBtnStatus(obj, readButton);
+        });
+
+        deleteButton.addEventListener("click", (e) => {
+            let index = e.target.value;
+            myLibrary.bookCase.splice(index, 1);
+            bookElement = document.querySelectorAll(".book");
+            bookElement[index].remove();
+
+            for (let i = 0; i < myLibrary.bookCase.length; i++) {
+                myLibrary.bookCase[i].index = i;
+                this.deleteBtnArray[i].value = i;
+            }
+        });
+    }
+
+    readBtnStatus(obj, readButton) {
+        if (obj.read == "1") {
+            readButton.textContent = "Read✓";
+        } else {
+            readButton.textContent = "Read✗";
         }
-    });
+    }
 }
 
-createBook.addEventListener("click", (e) => {
-    e.preventDefault();
-    const author = document.querySelector("#author").value;
-    const title = document.querySelector("#title").value;
-    const pages = document.querySelector("#pages").value;
-    const readStatus = document.getElementsByName("read");
-
-    if (readStatus[0].checked) {
-        read = readStatus[0].value;
-    } else {
-        read = readStatus[1].value;
-    }
-
-    let newBook = new Book(author, title, pages, read);
-    addBookToLibrary(newBook);
-    createBookElement(author, title, pages, newBook);
-    console.log(myLibrary);
-
-    dialog.close();
-});
+const myScreen = new screenUpdate(document);
+const myLibrary = new Library();
